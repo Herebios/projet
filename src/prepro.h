cf9c01b32bfcde7295293f1e4fd1d5ea3480f2e4
Herebios <palacraft01@gmail.com>
2025-01-19 09:55:50 +0100

    base du jeu

---
+++ src/prepro.h
@@ -0,0 +1,44 @@
+/*Ratio important
+W = CARRE_W * LARGEUR_TUILE
+H = CARRE_H * HAUTEUR_TUILE
+*/
+#define W 1408
+#define H 768
+#define CARRE_W 64
+#define CARRE_H 64
+
+#define NB_ANIM 3//nb de textures pour un perso
+#define NB_PERS_MAX 1
+#define VITESSE_BASE 8
+#define NIVEAU_BASE 1
+#define VIE_BASE 0
+#define ATTAQUE_BASE 0
+#define DEFENSE_BASE 0
+#define MAGIE_BASE 0
+#define MANA_BASE 0
+
+#define NB_BIOMES 3//correspond à l'enum nom_biome
+#define NB_TEXTURES_BIOME 3//correspond à l'enum type_carre
+
+#define NB_CAT_OBJETS 3
+
+#define HAUTEUR_TUILE 12
+#define LARGEUR_TUILE 22
+#define NB_OBJETS_TUILE 1
+#define NB_PNJ_TUILE 1
+#define NB_ENNEMIS_TUILE 1
+
+#define HAUTEUR_MAP 1
+#define LARGEUR_MAP 2
+
+//différencier perso et biome dans nouv_texture
+#define TEXTURE_BIOME 0
+#define TEXTURE_PERSO 1
+
+#define jw 64
+#define jh 64
+#define xdep 100
+#define ydep 100
+
+#define endl putchar('\n')
+#define flush fflush(stdout)
