55b1e71bc6ba8d74380c1c6322e4fd8b9db1b98b
s2301908 <palacraft01@gmail.com>
2025-03-14 15:40:44 +0100

    .

---
+++ src/perso_cli.h
@@ -0,0 +1,11 @@
+#ifndef _PERSO_CLI_H_
+#define _PERSO_CLI_H_
+
+#include "sdl.h"
+#include "perso.h"
+typedef struct{
+	perso_t perso;
+	sdl_struct sdl;
+}perso_cli;
+
+#endif//_PERSO_CLI_H_
---
+++ src/sdl.h
@@ -0,0 +1,10 @@
+#ifndef _SDL_H_
+#define _SDL_H_
+
+#include "SDL2/SDL.h"
+typedef struct{
+	SDL_Rect rect;
+	SDL_Texture * texture;
+}sdl_struct;
+
+#endif//_SDL_H_
