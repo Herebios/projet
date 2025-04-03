9b090ce0d6933fc11ca794fe5944fbf76c973305
Esperluette <l.reverbel34@gmail.com>
2025-04-04 01:31:04 +0200

    patch error

---
+++ cli.c
@@ -0,0 +1,356 @@
+#include "cli_jeu.h"
+#include "inventaire.c"
+
+file * file_socket;
+
+//variables utilisées en global
+socket_struct client;
+int indice, nb_joueurs;
+
+/* Utilise va_arg pour envoyer facilement ce qu'on veut
+socket et ind en global*/
+void sendf(char * format, ...){
+	char buffer[BUFFERLEN], *pformat=format;
+	sprintf(buffer, "%d ", indice);
+	va_list va;
+	va_start(va, format);
+	while(*pformat)
+		switch(*pformat++){
+			case 'd':
+				sprintf(buffer+strlen(buffer), "%d ", va_arg(va, int));
+				break;
+			case 's':
+				sprintf(buffer+strlen(buffer), "%s ", va_arg(va, char*));
+				break;
+		}
+	va_end(va);
+	send(client.socket, buffer, strlen(buffer), 0);
+}
+
+/*Utilisé pour lire après nb espaces
+Voir switch main_serv*/
+char * data_skip(char * data, int nb){
+    char * skip=data + 1;
+    while(nb){
+        if(*skip++ == ' ')
+            nb--;
+    }
+    return skip;
+}
+
+int main_client(char * ip, int port, char * pseudo, classe_t classe) {
+	//setup socket
+	int error_code;
+	if((error_code=setup_client(ip, port)) > 0){
+		fermeture_client(error_code);
+		return error_code;
+	}
+	puts("setup client OK\n");flush;
+
+	file_socket = newFile();
+
+	int buffer_size;
+	char buffer[BUFFERLEN];
+
+	pthread_create(&client.thread, NULL, ecoute_thread, NULL);
+
+	//attente de lancement
+	char * data;
+	char endwhile=0;
+	while(!endwhile){
+		data=defiler(file_socket);
+		if(data){
+			puts(data);flush;
+			if(strcmp(data, "start")==0)
+				endwhile=1;
+			free(data);
+			data=NULL;
+		}
+		putchar('\n');
+		sleep(1);
+	}
+	if(data){
+		puts(data);
+		free(data);
+	}
+
+	int seed;
+	//Récupère son indice
+	sleep(1);
+	data=defiler(file_socket);
+	sscanf(data, "%d %d %d", &indice, &nb_joueurs, &seed);
+	free(data);
+	printf("Je suis le client %d/%d !\n", indice, nb_joueurs);
+
+	//serv init le perso
+	sendf("ds", classe, pseudo);
+
+	init_sdl();
+	init_jeu();
+
+	perso_t joueurs[nb_joueurs];
+	SDL_Texture * textures_joueurs[nb_joueurs][4];
+	SDL_Texture * textures_objets[NB_OBJETS];
+
+	init_joueurs_client(joueurs);
+	charger_sdl_joueurs(joueurs, textures_joueurs);
+	charger_sdl_objets(textures_objets);
+
+	puts("Les joueurs sont :");
+	for(int i=0; i<nb_joueurs; i++){
+        printf("ind %d ; classe %d ; nom %s ; equipe %d\n", joueurs[i].iperso, joueurs[i].classe, joueurs[i].nom, joueurs[i].equipe);
+    }
+	flush;
+	perso_t * j = joueurs + indice;//pointeur sur le joueur, plus rapide
+	char valide=1;
+	int compteur=0;
+
+	texture_tuile = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_TARGET, W, H);
+	tuile_t * tuile_courante = NULL;;
+
+	SDL_Event event;
+    const Uint8* clavier;
+	char isSPACEPress = 0;
+	time_t t = 0;
+	while(valide && client.online){//si le thread est fermé prématurément
+
+		while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT || event.key.keysym.sym==SDLK_m){
+                valide=0;
+				break;
+            }
+			if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE && !isSPACEPress && time(NULL) - t < 3){
+				isSPACEPress = 1;
+				t = time(NULL);
+			}
+			if (event.type == SDL_KEYUP && event.key.keysym.sym == SDLK_SPACE){
+				isSPACEPress = 0;
+			}
+        }
+		if (isSPACEPress){
+			attaqueBasique(j);
+		}
+		clavier=SDL_GetKeyboardState(NULL);	
+        Uint8 mask=clavier[SDL_SCANCODE_RIGHT] << 3 | clavier[SDL_SCANCODE_LEFT] << 2 | clavier[SDL_SCANCODE_DOWN] << 1 | clavier[SDL_SCANCODE_UP];
+		if(mask){
+			dir_t dir=j->dir;
+			changer_dir(j, mask);
+			if(j->dir != dir)
+				sendf("dd", JOUEUR_CHANGE_DIR, j->dir);
+
+			avancer(j);
+
+			//serv simule aussi de son côté
+		}else if(j->dir != nulldir){
+			j->dir=nulldir;
+			sendf("dd", JOUEUR_CHANGE_DIR, nulldir);
+		}
+
+		while(!fileVide(file_socket)){
+			data=defiler(file_socket);
+			printf("action '%s'\n", (char*)data);flush;
+			int action;
+			sscanf(data, "%d", &action);
+			switch(action){
+				case SPAWN_OBJET:{
+					int ind_o;
+					pos_t pos_tuile;
+					sscanf(data_skip(data, 1), "%d %d %d", &ind_o, &pos_tuile.x, &pos_tuile.y);
+					ajouter_objet_tuile(tuile_courante, ind_o, pos_tuile);
+					//!! ajout dans sdl_objet
+					break;
+				}
+				case JOUEUR_MV:{
+					int i;
+					sscanf(data_skip(data, 1), "%d", &i);
+					sscanf(data_skip(data, 2), "%d %d %d", &joueurs[i].rect.x, &joueurs[i].rect.y, (int*)&joueurs[i].dir);
+					break;
+				}
+				case JOUEUR_MV_TUILE:{
+					detruire_tuile(get_tuile_joueur(j), 1);
+
+					int nb_o, nb_j;
+					sscanf(data_skip(data, 1), "%d %d %d", &j->pos_map.x, &j->pos_map.y, &nb_o);
+					printf("Nouvelle tuile : %d %d\n", j->pos_map.x, j->pos_map.y);
+					tuile_courante = get_tuile_joueur(j);
+					int skip=4;
+					int ind;
+					pos_t pos;
+					for(int i=0; i<nb_o; i++){
+						sscanf(data_skip(data, skip + 3*i), "%d %d %d", &ind, &pos.x, &pos.y);
+					    ajouter_objet_tuile(tuile_courante, ind, pos);
+					}
+					skip=4+3*nb_o;
+					sscanf(data_skip(data, skip++), "%d", &nb_j);
+					for(int i=0; i<nb_j; i++){
+						sscanf(data_skip(data, skip), "%d %d %d", &ind, &pos.x, &pos.y);
+						printf("Joueur sur la tuile : %d (%s)\n", ind, ind==indice ? "moi" : "pas moi");
+						joueurs[ind].rect.x = pos.x;
+						joueurs[ind].rect.y = pos.y;
+						ajouter_joueur_tuile(tuile_courante, ind);
+						skip+=3;
+					}
+				    charger_tuile(tuile_courante);
+					printf("taille liste : %d\n", taille_liste(tuile_courante->liste_joueurs));
+					break;
+				}
+				case ADD_JOUEUR_TUILE:{
+					int ind;
+					sscanf(data_skip(data, 1), "%d", &ind);
+					ajouter_joueur_tuile(tuile_courante, ind);
+					printf("Add joueur %d\n", ind);
+					break;
+				}
+				case RM_JOUEUR_TUILE:{
+					int ind;
+					sscanf(data_skip(data, 1), "%d", &ind);
+					retirer_joueur_tuile(tuile_courante, ind);
+					printf("Rm joueur %d\n", ind);
+					break;
+				}
+			}
+			free(data);
+		}
+		compteur += DELAY;
+
+	//affichage
+		//fond
+		afficher_tuile();
+		
+		//joueurs
+		for(tete_liste(tuile_courante->liste_joueurs); !hors_liste(tuile_courante->liste_joueurs); suivant_liste(tuile_courante->liste_joueurs)){
+			int ind = *(int*)get_liste(tuile_courante->liste_joueurs);
+			SDL_RenderCopy(renderer, textures_joueurs[ind][joueurs[ind].dir % 4], NULL, &joueurs[ind].rect);
+			if(compteur % 1000 == 0){
+                printf("J %d : tuile %d %d | position %d %d\n", ind, joueurs[ind].pos_map.x, joueurs[ind].pos_map.y, joueurs[ind].rect.x, joueurs[ind].rect.y);
+			}
+		}
+		//objets
+		for(tete_liste(tuile_courante->liste_objets); !hors_liste(tuile_courante->liste_objets); suivant_liste(tuile_courante->liste_objets)){
+			objet_tuile_t * obj = get_liste(tuile_courante->liste_objets);
+			SDL_RenderCopy(renderer, textures_objets[obj->objet->ind], NULL, &(SDL_Rect){obj->pos.x * CARRE_W, obj->pos.y * CARRE_H, CARRE_W, CARRE_H});
+		}
+		show_inventaire(renderer, j);
+
+
+
+		ecran();
+		SDL_Delay(DELAY);
+	}
+	send(client.socket, "!", 1, 0);
+
+	while(client.online){
+		puts("attente");
+		sleep(1);//attend fin ecoute_thread
+	}
+
+	detruire_objets_client(textures_objets);
+    detruire_joueurs_client(joueurs, textures_joueurs);
+	SDL_DestroyTexture(texture_tuile);
+	end(0);
+	fermeture_client(0);
+    return 0;
+}
+
+int main(int argc, char *argv[]){
+	if(argc == 5){
+		return main_client(argv[1], atoi(argv[2]), argv[3], atoi(argv[4]));
+	}else{
+		puts("ip port nom classe");
+		return 1;
+	}
+}
+
+void *ecoute_thread(void *arg){
+/*
+S'arrête quand le serveur envoie '!'
+Signal par pointeur qui arrête le main
+*/
+	int buffer_size;
+    char buffer[BUFFERLEN];
+    char valide=1;
+	client.online=1;
+	while(valide){
+		buffer_size = recv(client.socket, buffer, BUFFERLEN, 0);
+		if(buffer_size > 0){
+            buffer[buffer_size] = '\0';
+			printf("Recu : '%s'\n", buffer);flush;
+            switch(*buffer){
+                case '!':
+					valide=0;
+					break;
+                default:{
+					//on sépare les messages et on les met dans la file
+					char * debut, * fin;
+					debut=fin=buffer;
+					while(*fin)
+						if(*fin == ';'){
+							*fin=0;
+							enfiler(file_socket, debut, fin-debut+1);
+							fin++;
+							debut=fin;
+						}else fin++;
+				}
+            }
+        }else{
+			valide=0;
+			puts("Le serveur est mort, je crois.");
+		}
+	}
+	puts("fin ecoute thread\n");flush;
+	client.online=0;
+	return NULL;
+}
+void fermeture_client(int error_code){
+/*
+Libération correspondant au code renvoyé par le setup en cas d'échec,
+tout à la fin du programme, avec le code 0.
+*/
+	printf("fermeture\n");
+	if(file_socket)fileFree(&file_socket, free);
+	switch(error_code){
+case 0:
+case 3:
+	#ifdef _WIN32
+    closesocket
+    #else
+    close
+    #endif
+    (client.socket);
+case 2:
+    #ifdef _WIN32
+    WSACleanup();
+    #endif
+case 1:
+	break;
+	}
+}
+
+int setup_client(char * ip, int port){
+	int error_code;
+	#ifdef _WIN32
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa))
+		return 1;
+	#endif
+
+/* Si input de la forme ip:port
+	char ip[16];
+	char port[6];
+	char *p=strchr(argv[1], ':');
+	*p++='\0';
+	strcpy(ip,argv[1]);
+	strcpy(port,p);
+*/
+	if ((client.socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+		return 2;
+
+	client.addr.sin_family = AF_INET;
+	client.addr.sin_port = htons(port);
+	client.addr.sin_addr.s_addr = inet_addr(ip);
+
+	if(connect(client.socket, (struct sockaddr *)&client.addr, sizeof(client.addr)) == -1)
+		return 3;
+
+	return 0;
+}
---
+++ serv.c
@@ -0,0 +1,193 @@
+/**
+ * @file serv.c
+ * @author Baptiste
+ * @brief code principal du serveur
+ */
+#include "serv_socket.h"
+#include "serv_jeu.h"
+
+int main_server(int port, int nb_clients) {
+	//setup socket
+	int error_code;
+	if ((error_code = setup_server(port, nb_clients)) > 0){
+		//sortie avec libération adaptée
+		fermeture_server(error_code);
+		return error_code;
+	}
+	//le server est prêt, on va s'occuper des clients
+	puts("setup server OK\n");
+	printf("Nombre de clients : %d, pas de place libérée quand déconnexion\n", nb_clients);flush;
+	clients = malloc(sizeof(socket_struct) * nb_clients);
+
+	serv_file = newFile();
+
+	server.nb_clients=0;
+	server.server_struct.online=1;
+	server.nb_on=0;
+
+	pthread_create(&server.server_struct.thread, NULL, accept_thread, &nb_clients);
+	//nouv clients thread
+
+	char buffer[BUFFERLEN]="Il y a ";
+	while(server.nb_clients < nb_clients){
+		printf("%d", server.nb_clients);flush;
+		sprintf(buffer + 7, "%d", server.nb_clients);
+		strcat(buffer, " clients !;");
+		broadcast(buffer, -1);
+		sleep(1);
+	}
+	server.nb_on=nb_clients;
+	//à partir d'ici on peut utiliser server.nb_clients (global)
+	broadcast("start;", -1);
+
+	srand(time(0));
+	int random_seed = rand();
+	//Chaque client reçoit son indice et la seed random
+	for (int i=0; i<nb_clients; i++) {
+		sprintf(buffer, "%d %d %d;", i, nb_clients, random_seed);
+		send(clients[i].socket, buffer, strlen(buffer), 0);
+	}
+
+	init_jeu();
+
+	perso_t joueurs[nb_clients];
+	init_joueurs_server(joueurs, nb_clients);
+	send_joueurs_server(joueurs, nb_clients);
+	puts("Joueurs :");
+	for(int i=0; i<nb_clients; i++){
+		printf("ind %d , classe %d , nom %s , equipe %d\n", i, joueurs[i].classe, joueurs[i].nom, joueurs[i].equipe);
+	}
+	flush;
+	//init les tuiles avec les joueurs
+	for(int i=0; i<nb_clients; i++){
+		tuile_t * t=get_tuile_joueur(joueurs + i);
+		ajouter_joueur_tuile(t, i);
+	}
+	flush;
+
+	for(int i=0; i<nb_clients; i++){
+		maj_tuile(joueurs, i);
+	}
+	/*on[i] correspond à clients[i].online, pour mémoriser lequels sont off et ne les compter qu'une fois
+	on décrémente nb_on, qui est incrémenté quand le thread accepte un nouveau client
+	quand nb_on sera à 0 et premier_client à 1, le serveur s'arrêtera
+	*/
+	char * data;
+	int ind_j;//ind du joueur qui envoie
+	int action;
+	int compteur=0;
+
+	while (server.nb_on){
+		while(!fileVide(serv_file)){
+			data=defiler(serv_file);
+			sscanf(data, "%d %d", &ind_j, &action);
+			switch(action){
+				case JOUEUR_CHANGE_DIR:
+					sscanf(data_skip(data, 2), "%d", (int*)&joueurs[ind_j].dir);
+					break;
+				case ADD_OBJET_JOUEUR:{
+					int ind_o;
+					sscanf(data_skip(data, 2), "%d", &ind_o);
+					ajouter_objet_joueur(joueurs + ind_j, ind_o);
+					update_stats(joueurs + ind_j);
+					break;
+				}
+				case RM_OBJET_JOUEUR:{
+					int ind_inv, ind_o;
+					sscanf(data_skip(data, 2), "%d", &ind_inv);
+					ind_o = joueurs[ind_j].objets[ind_inv]->ind;
+					retirer_objet_joueur(joueurs + ind_j, ind_inv);
+					update_stats(joueurs + ind_j);
+
+					//on ajoute l'objet sur la tuile
+					pos_t pos_tuile;
+					position_perso(joueurs + ind_j, &pos_tuile);
+					spawn_objet((rarete_t)rand()%4, 1, ind_o, joueurs[ind_j].pos_map, pos_tuile);
+					break;
+				}
+				case BASIC_ATTACK:{
+					int ind_j;
+
+					pos_t tilePos;
+					perso_t *p = joueurs + ind_j;
+					position_perso(p , &tilePos);
+					tuile_t *tile = get_tuile_from_pos(tilePos);
+					tilePos.x += 2 * deplacement[p->dir].x;
+					tilePos.y += 2 * deplacement[p->dir].y;
+
+					size_t listeTaille = taille_liste(tile->liste_joueurs);
+					perso_t *victim;
+					pos_t victim_tilePos;
+					for (int i = 0; i < listeTaille; i++){
+						victim = get_liste(tile->liste_joueurs);
+						position_perso(victim, &victim_tilePos);
+						if (victim_tilePos.x == tilePos.x && victim_tilePos.y == tilePos.y){
+							printf("\nvictim!!!\n");
+						}
+						suivant_liste(tile->liste_joueurs);
+					}
+				}
+			}
+			free(data);
+		}
+
+		//gerer les deco
+		server.nb_on=server.nb_clients;
+		for (int i=0; i<nb_clients; i++){
+			if(!clients[i].online){
+				server.nb_on--;
+			}
+		}
+
+		//bouger les joueurs
+		for(int i=0; i<nb_clients; i++){
+			if(joueurs[i].dir != nulldir){
+				avancer(joueurs+i);
+				check_sortie_tuile(joueurs, i);//fait l'envoi
+
+				char buffer[16];
+				sprintf(buffer, "%d %d %d %d %d;", JOUEUR_MV, i, joueurs[i].rect.x, joueurs[i].rect.y, joueurs[i].dir);
+				//envoie aux joueurs la position des autres
+				tuile_t * tuile = get_tuile_joueur(joueurs + i);
+		        for(tete_liste(tuile->liste_joueurs); !hors_liste(tuile->liste_joueurs); suivant_liste(tuile->liste_joueurs)){
+		            int ind = *(int*)get_liste(tuile->liste_joueurs);
+					send(clients[ind].socket, buffer, strlen(buffer), 0);
+				}
+			}
+		}
+
+		//evenements
+		if(compteur % 1000 == 0){
+			for(int i=0; i<nb_clients; i++){
+				printf("J %d : tuile %d %d | position %d %d\n", i, joueurs[i].pos_map.x, joueurs[i].pos_map.y, joueurs[i].rect.x, joueurs[i].rect.y);
+			}
+		}
+		if(compteur >= 5000){
+			puts("Spawn obj\n");flush;
+			//paramètres temporaires
+			int ind; pos_t pos1, pos2;
+			spawn_objet((rarete_t)rand()%4, 0, ind, pos1, pos2);
+			compteur=0;
+		}else
+			compteur+=DELAY;
+
+		SDL_Delay(DELAY);
+	}
+	puts("fin boucle");flush;
+
+	detruire_joueurs_server(joueurs, nb_clients);
+	end(0);
+	//libération complète
+	fermeture_server(0);
+	return 0;
+}
+
+int main(int argc, char *argv[]){
+	if(argc != 2){
+		puts("pas de port");
+		return 1;
+	}
+	if(atoi(argv[1]) > 1024)
+		main_server(atoi(argv[1]), 2);
+	return 0;
+}
