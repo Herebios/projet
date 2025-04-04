593f64fcaf55a717e3dbe4e99005a578d559935b
Esperluette <l.reverbel34@gmail.com>
2025-04-04 23:58:52 +0200

    fin

--- src/cast.h
+++ src/cast.h
@@ -1,6 +1,12 @@
 #ifndef __CAST__
 #define __CAST__
 
+/**
+ * @file cast.h
+ * @author Lucas
+ * @brief Structure et fonction pour la généricité de la liste des différents type d'envoie de compétences.
+ */
+
 #include <time.h>
 
 #include "def.h"
@@ -13,6 +19,10 @@ typedef struct {
 	struct timespec lastUpdate;
 } cast_t;
 
+/**
+ * @fn int comparer(unsigned short int *e1, cast_t *e2)
+ * @brief Compare l'id d'un cast avec celui d'un autre cast
+ */
 int comparer(unsigned short int *e1, cast_t *e2);
 int comparer_cb(void *e1, void *e2);
 
